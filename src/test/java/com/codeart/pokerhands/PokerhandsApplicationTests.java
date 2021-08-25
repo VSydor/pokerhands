a649813557ef8369a58509d4a38e4456bc0f6fd0
vsydor <volodymyrsy@ext.inditex.com>
2021-08-25 13:19:51 +0300

    Initial commit

---
+++ src/test/java/com/codeart/pokerhands/PokerhandsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeart.pokerhands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerhandsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
